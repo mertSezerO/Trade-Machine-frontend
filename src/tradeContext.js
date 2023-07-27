87de242c40cd4edfddaf0a83aa9a81af656ea8d7
mertSezerO <117273151+mertSezerO@users.noreply.github.com>
2023-07-27 14:51:56 +0300

    Starting point for Context Refactorization

--- src/tradeContext.js
+++ src/tradeContext.js
@@ -2,8 +2,6 @@ import { createContext } from 'react';
 
 const TradeContext = createContext()
 export default function TradeProvider({ children }) {
-    // const [toggle, setToggle] = useState(false);
-    // const [teamsList, setTeamsList] = useState([]);
 
     const state = {
         tradeTeams,
